066bdb129e8d5404c87de642175fae3fa8102abe
Oliver <oliver-white@live.com>
2020-06-16 22:16:08 +1000

    Created smoother camera movement by changing the controls. Added the ability to use arrow keys. Added speed variable to control camera speed.

--- camera.js
+++ camera.js
@@ -3,6 +3,8 @@ let canvas;
 let camera;
 let context;
 let circles;
+const speed = 50;
+
 function setup()
 {
     console.log('hello');
@@ -41,6 +43,11 @@ function drawCircle(circle, context)
     let x = circle.x - camera.x;
     let y = circle.y - camera.y;
     context.arc(x,y, circle.radius, 0, 2 * Math.PI);
+    let xChange = Math.random() < 0.5 ? -1: 1;
+    let yChange = Math.random() < 0.5 ? -1: 1;
+    circle.y += xChange;
+    circle.x += yChange;
+    context.fill();
     context.closePath();
     context.stroke();
 }
@@ -48,33 +55,61 @@ function drawCircle(circle, context)
 function draw ()
 {
     context.clearRect(0, 0, document.querySelector("#myCanvas").width, document.querySelector("#myCanvas").height)
+    let count = 0;
+    circles.forEach(function(circle) {
+         if(Math.abs(circle.x - camera.x) < camera.xRange && Math.abs(circle.y - camera.y) < camera.yRange){ drawCircle(circle, context); count++;}
+         
+        } );
+    console.log("Circles Drawn: " + count);
+    camera.x += (cameraControlStates["Right"] ? speed : 0);
+    camera.x += (cameraControlStates["Left"] ? -speed : 0);
+    camera.y += (cameraControlStates["Up"] ? -speed : 0);
+    camera.y += (cameraControlStates["Down"] ? speed : 0);
+
     
-    circles.forEach(function(circle) { if(Math.abs(circle.x - camera.x) < camera.xRange && Math.abs(circle.y - camera.y) < camera.yRange){ drawCircle(circle, context)};} );
-   
+    
+
     window.requestAnimationFrame(draw);
     
 }
-
+let cameraControlStates = 
+{
+    "Up": false,
+    "Left" : false,
+    "Down" : false,
+    "Right" : false
+};
 const keyMap = 
 {
-    39 : 'right',
-    37 : 'left',
-    38 : 'up',
-    40 : 'down'
+    "KeyD" : 'Right',
+    "ArrowRight" : 'Right',
+    "ArrowLeft" : 'Left',
+    "KeyA" : 'Left',
+    
+    "KeyW" : 'Up',
+    "ArrowUp" : 'Up',
+    "KeyS" : 'Down',    
+    "ArrowDown" : 'Down'
 };
-function keydown(event)
+function keyDown(event)
 {
-    if(keyMap[event.keyCode] == 'left')
-    {
-        camera.x -= 10;
-    }
-    else if(keyMap[event.keyCode] == 'right')
-    {
-        camera.x += 10;
-    }
+    console.log("KeyDown")
+    cameraControlStates["Up"] = keyMap[event.code] == 'Up' ? true : cameraControlStates["Up"];
+    cameraControlStates["Right"] = keyMap[event.code] == 'Right' ? true : cameraControlStates["Right"];
+    cameraControlStates["Down"] = keyMap[event.code] == 'Down' ? true : cameraControlStates["Down"];
+    cameraControlStates["Left"] = keyMap[event.code] == 'Left' ? true : cameraControlStates["Left"];
+}
+function keyUp(event)
+{
+    console.log("KeyUp");
+    cameraControlStates["Up"] = keyMap[event.code] == 'Up' ? false : cameraControlStates["Up"];
+    cameraControlStates["Right"] = keyMap[event.code] == 'Right' ? false : cameraControlStates["Right"];
+    cameraControlStates["Down"] = keyMap[event.code] == 'Down' ? false : cameraControlStates["Down"];
+    cameraControlStates["Left"] = keyMap[event.code] == 'Left' ? false : cameraControlStates["Left"];
 }
 
-window.addEventListener('keydown', keydown);
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
 class Circle{
     constructor(xCenter, yCenter, radius){
         this.x = xCenter;
